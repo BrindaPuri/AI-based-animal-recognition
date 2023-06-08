fceee78ebce888c3e0a16650479ca11f35b75534
DrinkableBook <97373341+DrinkableBook@users.noreply.github.com>
2023-06-08 04:17:26 -0700

    Update ML.py
    no more global dec of weights.
    new dict format

--- flask-webapp/DeployAnmialDetection/animalDetection/backend/ML.py
+++ flask-webapp/DeployAnmialDetection/animalDetection/backend/ML.py
@@ -6,48 +6,57 @@ import torch
 from pytorch_pretrained_vit import ViT
 import pandas as pd
 from PIL import Image
-from app import BASEDIR, BACKENDDIR, IMAGEDIR
 import torchvision
 
-WEIGHTS_PATH = os.path.join(BACKENDDIR,'weights')
+#Directories
+WEIGHTS_PATH = 'weights/'
 
-#YOLOv8 Declaration
-yolov8 = YOLO(WEIGHTS_PATH+'yolov8.pt')
+#model names
 
-#Resnet model Declaration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-resnet = torchvision.models.resnet50(weights=True)
-resnet = torch.nn.DataParallel(resnet)
-resnet = resnet.to(device)
-resnet.load_state_dict(torch.load(WEIGHTS_PATH + 'resnet.pth', 
-                                  map_location=device))
-resnet.eval()
+def initialize_weights():
+    #YOLOv8 Declaration
+    yolov8 = YOLO(WEIGHTS_PATH + 'yolov8.pt')
 
-#ViT Model Declarations
-model_name = 'B_16_imagenet1k'
-vit = ViT(model_name, pretrained=True)
-vit.eval()
+    #Resnet model Declaration
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    resnet = torchvision.models.resnet50(weights=True)
+    resnet = torch.nn.DataParallel(resnet)
+    resnet = resnet.to(device)
+    resnet.load_state_dict(torch.load(WEIGHTS_PATH + 'resnet.pth', 
+                                    map_location=device))
+    resnet.eval()
 
-def predict(images):    
+    #ViT Model Declarations
+    model_name = 'B_16_imagenet1k'
+    vit = ViT(model_name, pretrained=True)
+    vit.eval()
+    return yolov8,resnet,vit
+
+def predict(images):
+    yolov8,resnet,vit = initialize_weights()    
     my_dict= {}
     if len(images) > 0:
         for image in images:
             #save as PiL
             image = Image.open('images/'+image.filename)
             print('--------',image.filename,'--------')
-            yolo_results = YoloPredict(yolo8, image, 0.25)
-            arr=[image.filename,0,0,0,0,0,0,0,0,0]
+            yolo_results = YoloPredict(yolov8, image, 0.25)
+            vit_res =  [0,0]
+            resnet_res = [0,0]
             #check if animal is detected
             if yolo_results[4] != 0:
                 print("animal detected: ", yolo_results[4]*100)
-                vit_res = ViTPredict(image,0.25)
+                vit_res = ViTPredict(vit,image,0.25)
                 resnet_res = Resnet_predict(resnet,image,0.25)
                 #resnet_res = [0,0]
                 #load results
-                arr = [yolo_results[0], yolo_results[1], 
-                                yolo_results[2], yolo_results[3], 
-                                yolo_results[4], resnet_res[0].item(), 
-                                resnet_res[1], vit_res[1],vit_res[0]]
+                field_names  =  ['x1','y1','x2','y2','Yolo_conf','Res_label','Res_conf'
+                                 ,'ViT_label','ViT_conf']
+            arr = {'x1':yolo_results[0], 'y1':yolo_results[1], 
+                            'x2':yolo_results[2], 'y2':yolo_results[3], 
+                            'Yolo_conf':yolo_results[4], 'Res_label':resnet_res[0], 
+                            'Res_conf':resnet_res[1], 'ViT_label':vit_res[0],
+                            'ViT_conf':vit_res[1]}
             
             print(arr,'\n')
             my_dict[image.filename] =  arr
@@ -76,7 +85,7 @@ def Resnet_predict(model, image, conf):
 
     _, index = torch.max(out, 1)
     percentage = torch.nn.functional.softmax(out, dim=1)[0]
-    
+
     if percentage[index[0]].item() < conf:
         return[0,0]
     return [index[0],percentage[index[0]].item()]
@@ -94,7 +103,7 @@ def YoloPredict(model, image, conf_low):
         return [0,0,0,0,0,0]
     
 
-def ViTPredict(image,conf_low):
+def ViTPredict(vit,image,conf_low):
     tfms = transforms.Compose([transforms.Resize(vit.image_size), 
                                transforms.ToTensor(), 
                                transforms.Normalize([0.5, 0.5, 0.5], 
@@ -111,3 +120,4 @@ def ViTPredict(image,conf_low):
     if prob < conf_low:
         return [0,0]
     return [idx, prob]
+
