2c962b8130ef3fd83f8d6a26c114587508b72869
Sanskriti Jain <60052484+sanjain13@users.noreply.github.com>
2023-06-08 07:55:37 -0700

    Update App.test.js

--- flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/App.test.js
+++ flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/ECS 193, Winter and Spring 2023/i)).toBeInTheDocument();
+  expect(screen.getByText(/Welcome to the Animal Recognition AI Pipeline./i)).toBeInTheDocument();
+
 });
+
+test("select images upload visible", async () => {
+  const { container } = render(<App/>);
+  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+  const boxes = container.getElementsByClassName('container');
+  console.log(boxes.length); 
+  expect(boxes.length).toBe(2);
+})
