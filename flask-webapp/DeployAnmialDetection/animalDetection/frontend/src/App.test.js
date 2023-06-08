14fa96472de5d321a75826c84be183b9ca68aab8
Brinda Puri <45851150+BrindaPuri@users.noreply.github.com>
2023-06-08 09:35:25 -0700

    Update App.test.js
    Added test to isolate test components

--- flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/App.test.js
+++ flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/App.test.js
@@ -1,5 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { unmountComponentAtNode } from "react-dom";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 test('renders app', () => {
   render(<App />);
