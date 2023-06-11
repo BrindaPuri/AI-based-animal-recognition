2fc2417bead1f7d2c322cb454c8e44448ee325e6
Sanskriti Jain <60052484+sanjain13@users.noreply.github.com>
2023-06-11 15:38:24 -0700

    Update InfoText.test.js

--- flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/__tests__/InfoText.test.js
+++ flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/__tests__/InfoText.test.js
@@ -17,9 +17,12 @@ test('renders hidden info text', () => {
     expect(boxes.length).toBe(1);
   });
 
-test('is there hidden text', () => {
+test('is there hidden', () => {
     render(<InfoText />);
     // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
-    const hidden = screen.getByTestId('info');
-    expect(hidden).toBeInTheDocument()
-  });
+    const hidden = screen.getByText(/ECS 193, Winter and Spring 2023/i);
+    expect(hidden).toBeInTheDocument();
+    // fireEvent.click(button);
+    // expect(hidden).toBe(<div class="hidden" data-testid="info"><p>created by Shuban Ranganath, Zhantong Qiu, Brinda Puri, and Sanskriti Jain</p><p>ECS 193, Winter and Spring 2023</p></div>);
+    // expect(hidden.innerHTML).toBe("<p>created by Shuban Ranganath, Zhantong Qiu, Brinda Puri, and Sanskriti Jain</p><p>ECS 193, Winter and Spring 2023</p>");
+});
