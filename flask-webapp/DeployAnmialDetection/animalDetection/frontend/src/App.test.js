a8d31d05039bb0528cd39b2212fc27c45e0610f9
Sanskriti Jain <60052484+sanjain13@users.noreply.github.com>
2023-06-13 17:26:42 -0700

    fix App.test.js, was not working with window

--- flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/App.test.js
+++ flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/App.test.js
@@ -1,3 +1,4 @@
+import './mockJsdom'
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import { unmountComponentAtNode } from "react-dom";
@@ -17,16 +18,28 @@ afterEach(() => {
 });
 
 test('renders app', () => {
+  window.URL.createObjectURL = jest.fn();
   render(<App />);
-  expect(screen.getByText(/ECS 193, Winter and Spring 2023/i)).toBeInTheDocument();
-  expect(screen.getByText(/Welcome to the Animal Recognition AI Pipeline./i)).toBeInTheDocument();
-
+  expect(screen.getByText("Animal Recognition AI Pipeline")).toBeInTheDocument();
+  expect(screen.getByText("Identify animal species by uploading your own dataset.")).toBeInTheDocument();
+  expect(screen.getByText("created by Shuban Ranganath, Zhantong Qiu, Brinda Puri, and Sanskriti Jain")).toBeInTheDocument();
+  expect(screen.getByText("ECS 193, Winter and Spring 2023")).toBeInTheDocument();
 });
 
-test("check for two containers", async () => {
+test("check for top container", async () => {
+  window.URL.createObjectURL = jest.fn();
+  const { container } = render(<App/>);
+  // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
+  const boxes = container.getElementsByClassName('topcontainer');
+  console.log(boxes.length); 
+  expect(boxes.length).toBe(1);
+})
+
+test("check for bottom container", async () => {
+  window.URL.createObjectURL = jest.fn();
   const { container } = render(<App/>);
   // eslint-disable-next-line testing-library/no-container, testing-library/no-node-access
   const boxes = container.getElementsByClassName('container');
   console.log(boxes.length); 
-  expect(boxes.length).toBe(2);
+  expect(boxes.length).toBe(1);
 })
