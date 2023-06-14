47fff2f9f7df18be2348b76f6e431f5f900aa1f1
Sanskriti Jain <60052484+sanjain13@users.noreply.github.com>
2023-06-14 11:51:04 -0700

    added tests for error messages after selecting no images

--- flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/__tests__/Logos.test.js
+++ flask-webapp/DeployAnmialDetection/animalDetection/frontend/src/__tests__/Logos.test.js
@@ -25,3 +25,44 @@ test('renders logo', () => {
   console.log(boxes.length); 
   expect(boxes.length).toBe(4);
 });
+
+test('button must have src = "image.png" and alt = "logo-logo"', () => {
+  render(<Logos />);
+  const logo = screen.getByRole('presentation', { name: "image"});
+  expect(logo).toHaveAttribute('src', 'image.png');
+  expect(logo).toHaveAttribute('alt', 'logo-logo');
+});
+
+test('click select images button', () => {
+  render(<Logos />);
+  const imageLogo = screen.getByRole('presentation', { name: "image"});
+  fireEvent.click(imageLogo);
+  expect(screen.getByText("0 images have been selected.")).toBeInTheDocument();
+});
+
+test('click select images button and error on upload', () => {
+  render(<Logos />);
+  const imageLogo = screen.getByRole('presentation', { name: "image"});
+  fireEvent.click(imageLogo);
+  const uploadLogo = screen.getByRole('presentation', { name: "out"});
+  fireEvent.click(uploadLogo);
+  expect(screen.getByText("You have not completed the select images step. Please make sure to complete that first.")).toBeInTheDocument();
+});
+
+test('click select images button and error on classify', () => {
+  render(<Logos />);
+  const imageLogo = screen.getByRole('presentation', { name: "image"});
+  fireEvent.click(imageLogo);
+  const classifyLogo = screen.getByRole('presentation', { name: "scan"});
+  fireEvent.click(classifyLogo);
+  expect(screen.getByText("You have not completed the select images step. Please make sure to complete that first.")).toBeInTheDocument();
+});
+
+test('click select images button and error on download', () => {
+  render(<Logos />);
+  const imageLogo = screen.getByRole('presentation', { name: "image"});
+  fireEvent.click(imageLogo);
+  const downloadLogo = screen.getByRole('presentation', { name: "download"});
+  fireEvent.click(downloadLogo);
+  expect(screen.getByText("You have not completed the select images step. Please make sure to complete that first.")).toBeInTheDocument();
+});
